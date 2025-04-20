21ffcb90559bc464ac136d18f92254df5083f935
Veda <nsengimanavedadom@gmail.com>
2025-04-20 04:23:25 +0200

    Update entrypoint.sh

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-# Fix permissions (especially if a volume is mounted here)
-mkdir -p api/src/models/v1_0
-chown -R "$(id -u)":"$(id -g)" api/src/models
-
-# Download model if needed
 if [ "$DOWNLOAD_MODEL" = "true" ]; then
     python download_model.py --output api/src/models/v1_0
 fi
 
-# Start the FastAPI app
-exec uv run --extra "$DEVICE" --no-sync python -m uvicorn api.src.main:app --host 0.0.0.0 --port 8880 --log-level debug
+exec uv run --extra $DEVICE --no-sync python -m uvicorn api.src.main:app --host 0.0.0.0 --port 8880 --log-level debug
